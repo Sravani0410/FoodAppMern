455d34d3a82ec42f17d94d1fd72d8637c9ad83ca
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-05-09 14:50:41 +0530

    add the routing using react-router-dom add the logo in navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import logo from "../assest/logo.png"
 const Header = () => {
   return (
-    <header>
+    // px-2 is mobile version and md:px-4 is desktop version
+    <header className='fixed shadow-md w-full h-16 px-2 md:px-4'>
     {/* Desktop */}
-    <div className=''>
-        <div>
-            <img src={logo}/>
+    <div className='flex items-center h-full'>
+        <div className='h-12'>
+            <img src={logo} className='h-full'/>
         </div>
     </div>
      {/* mobile */}
