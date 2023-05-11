3c23dd724bc722fe04fda9af8b7fadedc3f94b08
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-05-11 10:31:19 +0530

    add the number in add to cart and change the user and make the dropdown when user logo click by using tailwind css and function and usestate in navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assest/food_logo1.png";
 import { Link } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
+import { HiOutlineUserCircle } from "react-icons/hi";
 import { BsCartFill } from "react-icons/bs";
 const Header = () => {
+  const [showmenu, setShowMenu] = useState(false);
+  const handleShowMenu=()=>{
+    setShowMenu(prev=>!prev)
+  }
   return (
     // px-2 is mobile version and md:px-4 is desktop version
     <header className="fixed shadow-md w-full h-16 px-2 md:px-4 z-50">
@@ -21,11 +26,25 @@ const Header = () => {
             <Link to={"about"}>About</Link>
             <Link to={"contact"}>Contact</Link>
           </nav>
-          <div className="text-2xl text-slate-600">
+          <div className="text-2xl text-slate-600 relative">
             <BsCartFill />
+            <div className="absolute -top-1 -right-1 text-white bg-red-500 h-4 w-4 rounded-full m-0 text-sm text-center">
+              0
+            </div>
           </div>
-          <div className="text-2xl text-slate-600">
-            <FaUserAlt />
+          <div className="text-slate-600 " onClick={handleShowMenu}>
+            {/* <div className="border-2 border-solid border-slate-600 p-1 rounded-full">
+            <FaUserAlt/>
+            </div> */}
+            <div className="text-3xl cursor-pointer" >
+              <HiOutlineUserCircle />
+            </div>
+            {showmenu && (
+              <div className="absolute right-2 bg-white py-2 px-2 shadow drop-shadow">
+                <p className="whitespace-nowrap cursor-pointer">New Product</p>
+                <p className="whitespace-nowrap cursor-pointer">Login</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
