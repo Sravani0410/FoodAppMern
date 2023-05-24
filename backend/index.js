851e083f883ad9115973056eca9e23acf2363beb
Sravani Sandhya Devi Pyla <sandhyapyla.8@gmail.com>
2023-05-24 11:24:13 +0530

    install the libraries and connect to frontend and console and check data is comming or not

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,19 @@
-console.log("React app");
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "10mb" }));
+
+const PORT = process.env.PORT || 8080;
+app.get("/", (req, res) => {
+  res.send("server is connected");
+});
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+});
+app.listen(PORT, () => {
+  console.log("Server is Running at Port:", PORT);
+});
